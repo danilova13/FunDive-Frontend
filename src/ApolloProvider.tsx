f165bac1eb572cb5ee11de11715c879f0731f102
Anna Danilova <anyadanilova13@gmail.com>
2023-12-07 15:26:22 -0500

    working on context

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -3,17 +3,27 @@ import {
     ApolloProvider as Provider, 
     ApolloClient, 
     InMemoryCache, 
-    createHttpLink
+    createHttpLink,
 } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
 import App from "./App";
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:8000/graphql'
 })
+
+const authLink = setContext((_, { header }) => {
+    return { 
+        headers: {
+            ...header,
+            authorization: localStorage.getItem('jwtToken') || ""
+        }
+    }
+});
     
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Home = () => {
     return (
         <div>
-            Login
+            <h1>Homepage</h1>
         </div>
     );
 };
 
-export default Login;
+export default Home;
