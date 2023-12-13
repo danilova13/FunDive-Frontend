82f1d2bcc9b64ef1016e400e89806158860e1870
Anna Danilova <anyadanilova13@gmail.com>
2023-12-13 11:49:29 -0500

    add login page, add logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
+import { AuthContext } from '../context/AuthContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+    let navigate = useNavigate();
+    // get user and logout properties from the context
+    const { user, logout } = useContext(AuthContext);
+
+    // logout function that logout button can call 
+    const onLogout = () => {
+        // call logout function created in the context
+        logout();
+        navigate('/');
+    }
+    
+    console.log(user);
+
     return (
         <Box sx={{flexGrow: 1}}>
             <AppBar position='static'>
@@ -10,8 +26,19 @@ function Navbar() {
                         <Link to="/" style={{textDecoration: "none", color: "white"}}>FunDive</Link>
                     </Typography>
                     <Box alignItems="right" sx={{flexGrow: 1, textAlign: "right"}}>
-                        <Link to="/login" style={{textDecoration: "none", color: "white", marginRight: "10px"}}>Login</Link>
-                        <Link to="/register" style={{textDecoration: "none", color: "white"}}>Register</Link>
+                        { user ? 
+                            // if user exists give access to logout button
+                            <>
+                                <Button style={{textDecoration: "none", color: "white"}} onClick={onLogout}>Logout</Button>
+                            </>
+                        :
+                            // if user doesn't exist give access to login and register links
+                            <>
+                                <Link to="/login" style={{textDecoration: "none", color: "white", marginRight: "10px"}}>Login</Link>
+                                <Link to="/register" style={{textDecoration: "none", color: "white"}}>Register</Link>
+                            </>    
+                        }
+                        
                     </Box>
                 </Toolbar>
             </AppBar>
